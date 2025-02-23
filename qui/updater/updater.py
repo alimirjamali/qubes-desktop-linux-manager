1af77ee6de9ef4da5f16bbefcb3fcf0dbb92f364
Ali Mirjamali <ali@mirjamali.com>
2025-02-23 18:37:30 +0330

    Assure Update window width does not break OpenQA
    Refrence: https://github.com/QubesOS/qubes-desktop-linux-manager/pull/246#issuecomment-2676911529

--- qui/updater/updater.py
+++ qui/updater/updater.py
@@ -228,6 +228,7 @@ class QubesUpdater(Gtk.Application):
         # Wide enough for details section to show update progress.
         # But still less than 1024 pixels to do not break OpenQA tests.
         width = max(width, 1000)
+        width = min(width, self.main_window.get_screen().get_width() - 12)
         height = min(
             int(width * 1.2), self.main_window.get_screen().get_height() - 48
         )
